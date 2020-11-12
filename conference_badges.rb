d43064aa5119027aaf2b0176c009b6cccf582b39
David Gomara <davidgomara7@gmail.com>
2020-11-12 23:51:16 +0000

    commit tree 6725f28d4ab86b885923cef81b0bc9e803df6a59

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+    new_array = []
+    attendees.each{ |name| new_array.push("Hello, my name is #{name}.")}
+    new_array
+end
+
+def assign_rooms(attendees)
+    counter = 1
+    new_array = []
+    attendees.each do |name|
+        new_array.push("Hello, #{name}! You'll be assigned to room #{counter}!")
+        counter += 1
+    end
+    new_array
+end
+
+def printer(attendees)
+    batch_badge_creator(attendees).each { |people| puts people}
+    assign_rooms(attendees).each { |room| puts room}
+end
